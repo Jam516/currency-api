f914ef259151e799ec06eeb1f04f69533a76b912
Jam516 <kufuorkofi@gmail.com>
2025-03-08 12:14:29 +0000

    commit

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def transform_rates_format(rates_data):
             # Create new object and add to result array
             transformed_data.append({
                 "day": date,
-                "price": 1/rate,
+                "price": round(1/rate, 6),
                 "symbol": currency
             })
     
